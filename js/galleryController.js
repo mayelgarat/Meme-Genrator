c8deeb9cd23d21f41eca8f6e96cda7b668dae827
May Elgarat <mayelgarat1@gmail.com>
2021-12-16 16:06:35 +0200

    last

--- js/galleryController.js
+++ js/galleryController.js
@@ -8,16 +8,16 @@ function initGallery() {
 }
 
 function goToGallery() {
-document.querySelector('.gallery').classList.remove('hidden');
-document.querySelector('.editor').classList.add('hidden');
+    document.querySelector('.gallery').classList.remove('hidden');
+    document.querySelector('.editor').classList.add('hidden');
 
 }
+
 function renderImgs() {
     var str = ''
     for (var i = 1; i <= 18; i++) {
-        str += ` <div class="box box${i}"><img class="grid-img img-${i}" id = ${i} src="/meme-imgs (square)/${i}.jpg" onclick="setImg(this)">
+        str += ` <div class="box box${i}"><img class="grid-img img-${i}" id = ${i} src="./meme-imgs (square)/${i}.jpg" onclick="setImg(this)">
     </div>`
     }
     document.querySelector('.grid-container').innerHTML = str;
 }
-
