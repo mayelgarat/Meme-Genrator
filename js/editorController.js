83929a9f52546a2285370d1254044c3c08259c14
May Elgarat <mayelgarat1@gmail.com>
2021-12-18 13:53:25 +0200

    share

--- js/editorController.js
+++ js/editorController.js
@@ -149,7 +149,7 @@ function canvasClicked(ev) {
 }
 
 function share() {
-    const imgDataUrl = gElCanvas.toDataURL(image/png);
+    const imgDataUrl = gElCanvas.toDataURL();
     const shareData = {
         title: 'Canvas',
         text: 'Share Your Meme!',
